52ba6b1c08ce49482f336a0ba80cfe3123fd0c75
nam97viet <namhoangong97@gmail.com>
2024-01-15 14:40:05 -0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-let inputBtn = document.querySelector("#input-btn")
-inputBtn.addEventListener("click", function() {
+let myLeads = []
+const inputEl = document.querySelector("#input-el")
+const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
 
+//Add click event
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+//render
+function renderLeads(){
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++){
+        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `<li>
+        <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}
+        </a>
+        </li>`
+    }
+
+    ulEl.innerHTML = listItems
+}
